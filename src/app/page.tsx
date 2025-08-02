66fbdf2e26629491e24da00e83129bb391d989cc
cedrick-dev <ccalicdan23@gmail.com>
2025-08-02 16:44:33 +0800

    COLORS IN HEADER NOT FINISHED HEHEE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -315,7 +315,7 @@ export default function Home() {
                   className="rounded-lg shadow-xl"
                 />
               </div>
-              <div className="text-center md:text-left">
+              <div className="text-center md:text-left ">
                 <h2 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
                   {legacySection.title}
                 </h2>
