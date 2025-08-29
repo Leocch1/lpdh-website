9aeae2a8a093ffc0d668121d3ccba1aaf26307f7
Leo Louise Jimenez <bc.leolouise.jimenez@cvsu.edu.ph>
2025-08-29 10:51:13 +0800

    google analytics config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next';
+import Script from "next/script";
 import './globals.css';
 import { ConditionalLayout } from '@/components/conditional-layout';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: 'Las Pinas Doctors Hospital',
-  description: 'Las Piñas Doctors Hospital - Your health, our commitment.',
+  description: 'Official website of Las Pinas Doctors Hospital',
   icons: '/favicon.ico',
 };
 
@@ -16,7 +17,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body className="font-body antialiased bg-background text-foreground">
+         {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <ConditionalLayout>
           {children}
         </ConditionalLayout>
